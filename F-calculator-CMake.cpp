a8e2e661270afb8928bb3681e5d1072e59a280a1
k-zhou <kejin.zhou@aalto.fi>
2022-09-02 16:59:37 +0300

    Add project files.

---
+++ F-calculator-CMake.cpp
@@ -0,0 +1,12 @@
+﻿// F-calculator-CMake.cpp : Defines the entry point for the application.
+//
+
+#include "F-calculator-CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
